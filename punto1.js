caa9bea5943ce2957ccff3a54f705178699a7baa
caliche22 <caliche.a2@gmail.com>
2018-09-13 17:46:20 -0500

    cleanning branch

--- punto1.js
+++
@@ -1,9 +0,0 @@
-function cuadrador(sq){
-	var valoractual=Math.sqrt(sq)+1
-	var valorsiguiente=Math.sqrt(sq)+1
-	valorsiguiente*=valorsiguiente
-	if(valorsiguiente==Math.floor(valoractual*valoractual))
-	return valorsiguiente
-	else
-	return -1
-}
